522ac91d2240a991d0ee55ccc2649d93f0122ed9
Gerben Voogt <gerbenvoogt2@hotmail.com>
2023-02-25 14:41:28 +0100

    Added luasnip snippets for writing markdown

---
+++ config/nvim/LuaSnip/markdown.lua
@@ -0,0 +1,606 @@
+return {
+    --------------------
+    --- Enviroments  ---
+    --------------------
+    s({trig="begin", dscr="A LaTeX environment"},
+        fmt( -- The snippet code actually looks like the equation environment it produces.
+            [[
+              \begin{<>}
+                  <>
+              \end{<>}
+            ]],
+            { i(1), i(2), rep(1)},
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="align", dscr="A LaTeX align environment"},
+        fmt( -- The snippet code actually looks like the equation environment it produces.
+            [[
+              \begin{align}
+                  <>
+              \end{align}
+            ]],
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="gather", dscr="A LaTeX gather environment"},
+        fmt( -- The snippet code actually looks like the equation environment it produces.
+            [[
+            \begin{gather}
+            <>
+            \end{gather}
+            ]],
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="$$", dscr="A LaTeX gather environment", snippetType="autosnippet"},
+        fmt( 
+            [[
+            $$
+            <>
+            $$
+            ]],
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="mk", dscr="inline math", snippetType="autosnippet"},
+        fmt( 
+            "$<>$",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+
+    -----------------------------
+    --- Symbols and Functions ---
+    -----------------------------
+    s({trig="//", dscr="latex frac", snippetType="autosnippet"},
+        fmt( 
+            "\\frac{<>}{<>}",
+            { i(1), i(2) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="lim", dscr="latex lim function"},
+        fmt( 
+            "\\lim_{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="sin", dscr="latex sin function"},
+        fmt(
+            "\\sin(<>)",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="arcsin", dscr="latex arcsin function"},
+        fmt(
+            "\\arcsin(<>)",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="cos", dscr="latex cos function"},
+        fmt(
+            "\\cos(<>)",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="arccos", dscr="latex arccos function"},
+        fmt(
+            "\\arccos(<>)",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="tan", dscr="latex tan function"},
+        fmt(
+            "\\tan(<>)",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="arctan", dscr="latex arctan function"},
+        fmt(
+            "\\arctan(<>)",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="sqrt", dscr="latex sqrt function"},
+        fmt(
+            "\\sqrt{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="diff", dscr="derrivative"},
+        fmt(
+            "\\frac{d <>}{d <>}<>",
+            { i(1), i(2), i(3) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="pdiff", dscr="partial derrivative"},
+        fmt(
+            "\\frac{\\partial <>}{\\partial <>}<>",
+            { i(1), i(2), i(3) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="adiff", dscr="anti derrivative"},
+        fmt(
+            "\\int <>\\, d <>",
+            { i(1), i(2),},
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="sum", dscr="latex sum construct"},
+        fmt(
+            "\\sum_{<>}^{<>}",
+            { i(1), i(2) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="prod", dscr="latex prod construct"},
+        fmt(
+            "\\prod_{<>}^{<>} <>",
+            { i(1), i(2), i(3) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="int", dscr="latex integral construct"},
+        fmt(
+            "\\int_{<>}^{<>} <>\\, d<>",
+            { i(1), i(2), i(3), i(4) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="oint", dscr="latex boundary integral construct"},
+        fmt(
+            "\\oint_{<>} <>\\, d<>",
+            { i(1), i(2), i(3) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="in", dscr="latex in symbol"},
+        fmt(
+            "\\in <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="forall", dscr="latex forall symbol"},
+        fmt(
+            "\\forall <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="exists", dscr="latex exists symbol"},
+        fmt(
+            "\\exists <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="|->", dscr="mapsto"},
+        fmt(
+            "\\mapsto <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="->", dscr="rightarrow"},
+        fmt(
+            "\\rightarrow <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="=>", dscr="implies"},
+        fmt(
+            "\\Rightarrow <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="<=>", dscr="iff"},
+        fmt(
+            "\\Leftrightarrow <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig=">=", dscr="geq"},
+        fmt(
+            "\\geq <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="<=", dscr="leq"},
+        fmt(
+            "\\leq <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="!=", dscr="neq"},
+        fmt(
+            "\\neq <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="text", dscr="latex text in mathmode function"},
+        fmt( 
+            "\\text{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="bf", dscr="latex boldsymbol", snippetType="autosnippet"},
+        fmt( 
+            "\\mathbf{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="cdot", dscr="cdot", snippetType="autosnippet"},
+        fmt( 
+            "\\cdot <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="...", dscr="cdots", snippetType="autosnippet"},
+        fmt( 
+            "\\cdots <>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+
+
+    ---------------
+    --- LETTERS ---
+    ---------------
+    s({trig="RR", dscr="latex Real numbers symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\mathbb{R}<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="ZZ", dscr="latex Integer  numbers symbol", snippetType="autosnippet"},
+
+        fmt(
+            "\\mathbb{Z}<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="QQ", dscr="latex Rationals  numbers symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\mathbb{Q}<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="nabla", dscr="nabla"},
+        fmt( 
+            "\\nabla<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="partial", dscr="partial"},
+        fmt( 
+            "\\partial<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="ooo", dscr="infty", snippetType="autosnippet"},
+        fmt( 
+            "\\infty<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@a", dscr="latex alpha symbol", snippetType="autosnippet"},
+        fmt(
+            "\\alpha<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@b", dscr="latex beta symbol", snippetType="autosnippet"},
+        fmt(
+            "\\beta<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@g", dscr="latex alpha symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\gamma<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@d", dscr="latex delta symbol", snippetType="autosnippet"},
+        fmt(
+            "\\delta<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@D", dscr="latex Delta symbol", snippetType="autosnippet"},
+        fmt(
+            "\\Delta<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@e", dscr="latex epsilon symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\varepsilon<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@z", dscr="latex zeta symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\zeta<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@h", dscr="latex eta symbol", snippetType="autosnippet"},
+        fmt(
+            "\\eta<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@u", dscr="latex theta symbol", snippetType="autosnippet"},
+        fmt(
+            "\\theta<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@k", dscr="latex kappa symbol", snippetType="autosnippet"},
+        fmt(
+            "\\kappa<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@l", dscr="latex lambda symbol", snippetType="autosnippet"},
+        fmt(
+            "\\lambda<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@L", dscr="latex Lambda symbol", snippetType="autosnippet"},
+        fmt(
+            "\\Lambda<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@m", dscr="latex mu symbol", snippetType="autosnippet"},
+        fmt(
+            "\\mu<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@n", dscr="latex nu symbol", snippetType="autosnippet"},
+        fmt(
+            "\\nu<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@x", dscr="latex xi symbol", snippetType="autosnippet"},
+        fmt(
+            "\\xi<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@p", dscr="latex pi symbol", snippetType="autosnippet"},
+        fmt(
+            "\\pi<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@P", dscr="latex Pi symbol", snippetType="autosnippet"},
+        fmt(
+            "\\Pi<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@r", dscr="latex rho symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\rho<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@s", dscr="latex sigma symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\sigma<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@S", dscr="latex Sigma symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\Sigma<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@t", dscr="latex tau symbol", snippetType="autosnippet"},
+        fmt(
+            "\\tau<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@f", dscr="latex phi symbol", snippetType="autosnippet"},
+        fmt( 
+            "\\phi<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@F", dscr="latex Phi symbol", snippetType="autosnippet"},
+        fmt(
+            "\\Phi<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@y", dscr="latex psi symbol", snippetType="autosnippet"},
+        fmt(
+            "\\psi<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@Y", dscr="latex Psi symbol", snippetType="autosnippet"},
+        fmt(
+            "\\Psi<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@o", dscr="latex omega symbol", snippetType="autosnippet"},
+        fmt(
+            "\\omega<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="@O", dscr="latex Omega symbol", snippetType="autosnippet"},
+        fmt(
+            "\\Omega<>",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+
+    -----------------------------
+    --- Accents and Modifiers ---
+    -----------------------------
+    s({trig="bar", dscr="overline"},
+        fmt(
+            "\\overline{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="hat", dscr="hat"},
+        fmt(
+            "\\hat{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="dot", dscr="dot"},
+        fmt(
+            "\\dot{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="ddot", dscr="ddot"},
+        fmt(
+            "\\ddot{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig="vec", dscr="vec"},
+        fmt(
+            "\\vec{<>}",
+            { i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig = '([%a%)%]%}])^', regTrig = true, wordTrig = false, snippetType="autosnippet"},
+        fmt(
+            "<>^{<>}",
+            { f(function(_, snip) return snip.captures[1] end), i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig = '([%a%)%]%}])_', regTrig = true, wordTrig = false, snippetType="autosnippet"},
+        fmt(
+            "<>_{<>}",
+            { f(function(_, snip) return snip.captures[1] end), i(1) },
+            { delimiters = "<>" }
+        )
+    ),
+    s({trig = '([%a%)%]%}])ii', regTrig = true, wordTrig = false, snippetType="autosnippet"},
+      fmta(
+        "<>_{<>}",
+        {
+          f( function(_, snip) return snip.captures[1] end ),
+          t("i")
+        }
+      )
+    ),
+    s({trig = '([%a%)%]%}])jj', regTrig = true, wordTrig = false, snippetType="autosnippet"},
+      fmta(
+        "<>_{<>}",
+        {
+          f( function(_, snip) return snip.captures[1] end ),
+          t("j")
+        }
+      )
+    ),
+    s({trig = '([%a%)%]%}])nn', regTrig = true, wordTrig = false, snippetType="autosnippet"},
+      fmta(
+        "<>_{<>}",
+        {
+          f( function(_, snip) return snip.captures[1] end ),
+          t("n")
+        }
+      )
+    ),
+    s({trig = '([%a%)%]%}])np1', regTrig = true, wordTrig = false, snippetType="autosnippet"},
+      fmta(
+        "<>_{<>}",
+        {
+          f( function(_, snip) return snip.captures[1] end ),
+          t("n+1")
+        }
+      )
+    ),
+    s({trig = '([%a%)%]%}])00', regTrig = true, wordTrig = false, snippetType="autosnippet"},
+      fmta(
+        "<>_{<>}",
+        {
+          f( function(_, snip) return snip.captures[1] end ),
+          t("0")
+        }
+      )
+    ),
+}
+
