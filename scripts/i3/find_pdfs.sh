b2d6395e54a963e1b4fcaabceb7c391c046d12da
Gerben Voogt <gerbenvoogt2@hotmail.com>
2023-08-15 09:11:52 +0000

    fixed small bug in pdf opening tool

--- scripts/i3/find_pdfs.sh
+++ scripts/i3/find_pdfs.sh
@@ -1,14 +1,13 @@
-#!/bin/zsh
+#!/bin/bash
 
 pdf_files=$(/home/gerb/i3-scripts/find_pdfs)
 pdf_names=$(/home/gerb/i3-scripts/find_pdfs | /home/gerb/i3-scripts/filename.py)
 
-#
 # Use Rofi to select a PDF file by name
 selected_name=$(echo "${pdf_names[@]}" | rofi -dmenu -i -p " Select file")
 
 # Get the full path of the selected file
-selected_file=$(echo "$pdf_files" | grep -F "$selected_name")
+selected_file=$(echo "$pdf_files" | grep -F -m 1 "$selected_name")
 
 if [ -n "$selected_name" ]; then
     zathura "$selected_file" &
