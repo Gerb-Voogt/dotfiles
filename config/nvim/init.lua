d29ff312d29c8b2221a1f42e650478e0ecc4d3bf
Gerben Voogt <gerbenvoogt2@hotmail.com>
2023-12-10 14:45:15 +0000

    fixed indentation plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -116,11 +116,12 @@ require('lazy').setup({
 
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      -- char = '┊',
+      -- show_trailing_blankline_indent = false,
     },
   },
 
@@ -211,7 +212,6 @@ require('lazy').setup({
   'lervag/vimtex',
 
 }, {})
-
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
