6413e9b92930751874d4d86f70d168dec898cc44
unknown <gerbenvoogt2@hotmail.com>
2024-10-25 11:34:25 +0200

    Added marks and DAP

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -152,6 +152,11 @@ require('lazy').setup({
   --   -- Optional dependencies
   --   dependencies = { "nvim-tree/nvim-web-devicons" },
   -- },
+  { -- Marks plugin for better mark usage
+    "chentoast/marks.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
   { 'JuliaEditorSupport/julia-vim' },
   {
     'RaafatTurki/hex.nvim',
@@ -236,6 +241,73 @@ require('lazy').setup({
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+  },
+  {
+    "folke/zen-mode.nvim",
+  },
+
+  -- Setting up nvim debug adapter protocol
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "mfussenegger/nvim-dap-python",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+    },
+     config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
+
+      require("dapui").setup()
+      require("dap-python").setup("python3")
+
+
+      require("nvim-dap-virtual-text").setup {
+        -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+        display_callback = function(variable)
+          local name = string.lower(variable.name)
+          local value = string.lower(variable.value)
+          if name:match "secret" or name:match "api" or value:match "secret" or value:match "api" then
+            return "*****"
+          end
+
+          if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+          end
+
+          return " " .. variable.value
+        end,
+      }
+
+      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F13>", dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
   },
 },
 {})
@@ -500,6 +572,21 @@ require("lspconfig").julials.setup({
   end,
 })
 
+require('lspconfig').pyright.setup{
+    on_init = function(client)
+        client.config.settings.python = {
+            analysis = {
+                diagnosticSeverityOverrides = {
+                    reportUnusedVariable = "none",
+                },
+            },
+        }
+        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        return true
+    end,
+}
+vim.cmd [[highlight! link LspDiagnosticsVirtualTextHint Normal]]
+
 
 -- Setup neovim lua configuration
 require('neodev').setup()
@@ -599,7 +686,8 @@ vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.sw = 4
 vim.o.ts = 4
-vim.o.expandtab = true;
+vim.o.expandtab = true
+vim.opt.expandtab = true
 
 require('aerial').setup({
   on_attach = function(bufnr)
